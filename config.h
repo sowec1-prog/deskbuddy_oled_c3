416f72f0a8b9571a8add340181666e332b8ff4a1
sowec1-prog <sowec1@gmail.com>
2026-09-12 10:05:20 +0200

    Delete config.h

--- config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "config.h.example"
-
-#undef WIFI_SSID
-#define WIFI_SSID "Vodafone-2g"
-#undef WIFI_PASSWORD
-#define WIFI_PASSWORD "Stehlikova11"
-#undef OPENWEATHER_API_KEY
-#define OPENWEATHER_API_KEY "9a1c4e916a179dd7979d7f4ac82a9184"
